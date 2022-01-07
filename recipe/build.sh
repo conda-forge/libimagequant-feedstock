6d898a0fe07081b3eaf7bafadfa0a16b84184729
H. Vetinari <h.vetinari@gmx.com>
2022-01-07 19:42:36 +1100

    do not build static library (and switch to cmake to harmonize)

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-./configure --prefix=$PREFIX --with-openmp
-make -j$CPU_COUNT shared imagequant.pc
-make install
+set -ex
+
+mkdir build
+cd build
+
+# LIB_INSTALL_DIR & CMAKE_BINARY_DIR used by upstream CMakeLists.txt
+cmake \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DLIB_INSTALL_DIR:PATH=$PREFIX/lib \
+    -DCMAKE_BINARY_DIR:PATH=$PREFIX/bin \
+    ..
+
+cmake --build . --config Release
+
+cmake --install . --config Release
